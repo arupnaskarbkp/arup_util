3b63f16d7bc4f526213d359b7ae6e703b76ef1d4
Arup Naskar <40661263+arupnaskarbkp@users.noreply.github.com>
2022-09-11 09:03:54 +0530

    Initial commit

---
+++ lib/arup_util.dart
@@ -0,0 +1,7 @@
+library arup_util;
+import 'package:flutter/material.dart';
+export 'util/extensions/align_extension.dart';
+
+void initUtilities() async {
+  WidgetsFlutterBinding.ensureInitialized();
+}
